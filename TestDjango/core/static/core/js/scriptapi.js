7e2dc01ad6f8d263c2d694c0fc2817cec26e4321
kev-rojas <kev.rojasv@duocuc.cl>
2021-07-02 19:27:11 -0400

    cambios

--- TestDjango/core/static/core/js/scriptapi.js
+++ TestDjango/core/static/core/js/scriptapi.js
@@ -1,26 +1,12 @@
 $(document).ready(function() {
-
-    alert('paso1')
-
-    $.getJSON('127.0.0.1:8000/api/lista_usua', function(data) {
-
+    //alert('paso1')
+    $.getJSON('http://127.0.0.1:8000/api/lista_usua', function(data) {
         console.log(data)
-
         var Usuario = data;        
-
         $('#nombre').html(Usuario.nombre);
-
         $('#email').html(Usuario.email);
-
         $('#contraseña').html(Usuario.contraseña);
-
-
     }).fail(function() {
-
         console.log('Error al consumir la API!');
-
     });
-
-
-
 });
--- TestDjango/rest_usuario/urls.py
+++ TestDjango/rest_usuario/urls.py
@@ -3,6 +3,7 @@ from django.urls.resolvers import URLPattern
 from rest_usuario.views import lista_usua, detalle_usuario
 from rest_usuario.viewslogin import login
 
+
 urlpatterns = [
     path('lista_usua', lista_usua, name="lista_usua"),
     path('detalle_usuario/<id>', detalle_usuario, name="detalle_usuario"),
